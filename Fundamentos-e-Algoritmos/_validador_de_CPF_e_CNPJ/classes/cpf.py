6687f2b0cccfa7b057e67a76706e3d5a5db605b5
samuel <samuelbrito782@gmail.com>
2025-02-06 20:36:26 -0300

    inicio do desacoplamento de código

--- Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cpf.py
+++ Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cpf.py
@@ -1,4 +1,5 @@
 # Name: main.py
+from ..src.utils import _sanitize
 CPFLEN = 11
 
 class CPF:
@@ -6,17 +7,8 @@ class CPF:
         """
         Inicializa um objeto CPF, sanitizando o número informado e armazenando os dados.
         """
-        self.__cpf = self._sanitize_cpf(cpf)
+        self.__cpf = self._sanitize(cpf)
         self.valid = self._cpf_validator()
-
-    def _sanitize_cpf(self):
-        """
-        Remove pontos e traços do CPF e retorna apenas numeros
-        """
-        try:        
-            return self.cpf.replace('.', '').replace('-', '')
-        except Exception as error:
-            print(f"Error: {error}")
     
 
     def _cpf_validator(self) -> bool:
@@ -37,25 +29,8 @@ class CPF:
         """
         return len(self.cpf) == CPFLEN
 
-    def _cpf_validator_digit(self) -> bool:
-        result = 0
-        second_result = 0
-        try:
-            for _ in range (9):
-                    self.__new_cpf = self.__cpf[_] * (10 - _)
-                    result = sum(__new_cpf)%11
-                    if(result < 2):
-                        self.first_digit = 0
-                        return self.first_digit
-                    else:
-                        result = result - result
-                        return second_result    
-       
-        except Exception as error:
-            print(f'Error: {error}')
 
-
-    def _cpf_second_verificator_digit(self) : 
+    def _cpf_validator_digit(self) : 
         """
         Verifica se os dois últimos dígitos do CPF são válidos conforme o cálculo oficial.
         """
