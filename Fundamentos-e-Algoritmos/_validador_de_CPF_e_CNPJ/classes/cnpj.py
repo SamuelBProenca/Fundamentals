14b8a98cf664725c8429d83c65ba4576b3cd6743
samuel <samuelbrito782@gmail.com>
2025-02-07 22:53:46 -0300

    Definição e adição da classe cnpj

--- Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cnpj.py
+++ Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cnpj.py
@@ -1,2 +1,22 @@
+from src.utils import _len_verifyer, _sanitize, _digits_verifyer
 class CNPJ:
-    pass
+    def __init__(self, cnpj):
+        """
+        Inicializa um objeto CNPJ, sanitizando o número e validando automaticamente.
+        """
+        self.cnpj = _sanitize(cnpj)
+        self.valid = self._cnpj_validator()
+        
+    def _cnpj_validator(self) -> bool:
+        """
+        Valida o CNPJ verificando o tamanho e os dígitos verificadores.
+        """
+        try:
+            return _len_verifyer(self.cnpj, "cnpj") and _digits_verifyer(self.cnpj, "cnpj")
+        
+
+        
+        except Exception as error:
+            print(f"Erro na validação do CNPJ: {error}")
+            return False
+        
