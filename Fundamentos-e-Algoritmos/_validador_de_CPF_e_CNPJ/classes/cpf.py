4b9ccb36e084f8f14f839916bd26cb1e9b4ae53d
samuel <samuelbrito782@gmail.com>
2025-02-07 22:54:24 -0300

    Refatoração da classe e separação de responsabilidades

--- Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cpf.py
+++ Fundamentos-e-Algoritmos/_validador_de_CPF_e_CNPJ/classes/cpf.py
@@ -1,5 +1,5 @@
-# Name: main.py
-from ..src.utils import _sanitize
+# Name: cpf.py
+from src.utils import _sanitize, _len_verifyer, _digits_verifyer
 CPFLEN = 11
 
 class CPF:
@@ -7,42 +7,16 @@ class CPF:
         """
         Inicializa um objeto CPF, sanitizando o número informado e armazenando os dados.
         """
-        self.__cpf = self._sanitize(cpf)
+        self.__cpf = _sanitize(cpf)
         self.valid = self._cpf_validator()
-    
 
-    def _cpf_validator(self) -> bool:
-        """
-        Valida um CPF verificando se ele tem o tamanho correto e se os dígitos verificadores são válidos.
-        """
-        try: 
-            if not self._len_verifyer():
-                return False
-            return self.__cpf_validator_digit()
-
-        except Exception as error:
-            print(f'Error: {error}')
-            
-    def _len_verifyer(self) -> bool:
-        """
-        Verifica se o CPF tem o tamanho correto (11 dígitos)
-        """
-        return len(self.cpf) == CPFLEN
-
-
-    def _cpf_validator_digit(self) : 
+    def _cpf_validator_digit(self) -> bool: 
         """
         Verifica se os dois últimos dígitos do CPF são válidos conforme o cálculo oficial.
         """
-        first_digit = self._calculate_digit(self.cpf[:9], 10)
-        second_digit = self._calculate_digit(self.cpf[:9] + str(first_digit), 11)
-        return self.cpf[-2:] == f"{first_digit}{second_digit}"
-    
-    def _calculate_digit(self, cpf_part: str, weight: int ) -> int:
-        """
-        Calcula um dígito verificador do CPF.
-        """
-        result = sum(int(num) * (weight - i) for i, num in enumerate(cpf_part))
-        remainder = result % 11
-        return 0 if remainder < 2 else 11 - remainder
-     
+        try:
+            _len_verifyer(self.__cpf) and _digits_verifyer(self.__cpf, "cpf")
+        except Exception as error:
+            print(f"Erro na validação do CPF: {error}")
+            return False
+        
